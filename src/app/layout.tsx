294866ee57323181b8c9e0004371ebb58c272bf5
Artag/Chris <scristxyz@gmail.com>
2024-12-28 12:08:01 -0500

    fix de unos bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,14 @@
+'use client'
 import './globals.css'
 import { Poppins } from 'next/font/google'
-import { ThemeProvider } from "../components/components/theme-provider"
-import { Metadata } from 'next'
-import { SessionProvider } from 'next-auth/react'
+
+import { ThemeProvider } from "../components/components/theme-provider";
+import { SessionProvider } from 'next-auth/react';
+
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '600', '700'] })
 
-export const metadata: Metadata = {
-  title: 'VibrantShop - Tu tienda en línea',
-  description: 'Descubre productos increíbles en nuestra tienda vibrante y energética.',
-  openGraph: {
-    title: 'VibrantShop - Tu tienda en línea',
-    description: 'Descubre productos increíbles en nuestra tienda vibrante y energética.',
-    images: ['/og-image.jpg'],
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: 'VibrantShop - Tu tienda en línea',
-    description: 'Descubre productos increíbles en nuestra tienda vibrante y energética.',
-    images: ['/og-image.jpg'],
-  },
-}
+
 
 export default function RootLayout({
   children,
@@ -44,4 +32,3 @@ export default function RootLayout({
     </html>
   )
 }
-
