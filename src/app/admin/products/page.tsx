0e5511403c92390036b21a25bb3836829cfce64a
Artag/Chris <scristxyz@gmail.com>
2025-01-01 08:36:20 -0500

    act

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -86,7 +86,9 @@ export default function ProductsPage() {
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {unavailableProducts.map((product) => (
+             <FadeInTransition position="bottom">
             <ProductCard key={product.id} product={product} />
+            </FadeInTransition>
           ))}
         </div>
       </div>
