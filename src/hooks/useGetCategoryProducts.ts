60b008ddc6878d6c65a7349cba284550c4ac5782
Artag/Chris <scristxyz@gmail.com>
2025-05-01 21:24:26 -0500

    act

--- src/hooks/useGetCategoryProducts.ts
+++ src/hooks/useGetCategoryProducts.ts
@@ -40,7 +40,7 @@ export default function useGetCategoryProducts(categoryId: string) {
       } finally {
         setLoading(false);
       }
-    };
+    };  
  
     fetchProducts();
   }, [categoryId]);
